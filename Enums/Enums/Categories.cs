b432c13e533d42f7b17196bdce73ca73dfdc087f
Alejandro-cj <aloflow_345@hotmail.com>
2025-06-09 13:54:06 -0500

    categories

---
+++ Enums/Enums/Categories.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Enums
+{
+    public enum Categories
+    {
+        Housing,
+        Utilities,
+        Groceries,
+        Transportation,
+        Entertainment,
+        Medical,
+        Insurance,
+        Debt,
+        Education,
+        Savings,
+        Investments,
+        General
+    }
+}
